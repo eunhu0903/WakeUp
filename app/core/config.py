a872e50bd8dce4739405d17d668f35da0bec496e
eunhu0903 <eunhoo0903@naver.com>
2025-09-05 21:27:36 +0900

    feat(core): 환경변수 관리를 위한 Pydantic 설정 추가

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
