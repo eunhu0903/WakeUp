a8e2e3bc7a2ee04b7b9a28e53f2fde024a64adc8
eunhu0903 <eunhoo0903@naver.com>
2025-09-11 15:42:26 +0900

    fix(config): ACCESS_TOKEN_EXPIRE_MINUTES 타입을 str에서 int로 수정

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
